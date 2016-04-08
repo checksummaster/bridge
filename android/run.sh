f872c80539261f7bd581a66efa6c3253f5cf3c5e
Alexandre Lefebvre <alexandre.lefebvre@gmail.com>
2016-04-07 23:13:43 -0400

    make run.sh no dependency

--- android/run.sh
+++ android/run.sh
@@ -1,9 +1,51 @@
-cd minicap
-./run.sh &
-cd ..
-cd minitouch
-./run.sh &
-cd ..
+#!/usr/bin/env bash
+
+# Fail on error, verbose output
+#set -exo pipefail
+
+# Figure out which ABI and SDK the device has
+abi=$(adb shell getprop ro.product.cpu.abi | tr -d '\r')
+sdk=$(adb shell getprop ro.build.version.sdk | tr -d '\r')
+rel=$(adb shell getprop ro.build.version.release | tr -d '\r')
+
+# PIE is only supported since SDK 16
+if (($sdk >= 16)); then
+  bin=minicap
+  bin2=minitouch
+else
+  bin=minicap-nopie
+  bin2=minitouch-nopie
+fi
+
+# Create a directory for our resources
+dir=/data/local/tmp/minicap-devel
+adb shell "mkdir $dir 2>/dev/null"
+
+# Upload the binary
+echo --------------------
+echo upload binary
+echo --------------------
+adb push minicap/libs/$abi/$bin $dir
+adb push minitouch/libs/$abi/$bin2 /data/local/tmp/
+
+
+# Upload the shared library
+echo --------------------
+echo Upload the shared library
+echo --------------------
+if [ -e minicap/jni/minicap-shared/aosp/libs/android-$rel/$abi/minicap.so ]; then
+  adb push minicap/jni/minicap-shared/aosp/libs/android-$rel/$abi/minicap.so $dir
+else
+  adb push minicap/jni/minicap-shared/aosp/libs/android-$sdk/$abi/minicap.so $dir
+fi
+
+# Run!
+echo --------------------
+echo run
+echo --------------------
+adb shell /data/local/tmp/$bin2 &
+adb shell LD_LIBRARY_PATH=$dir $dir/$bin -P 1440x2560@480x800/90 &
+
 sleep 3
 
 #disable auto rotation
